101d60e57095f7b589c1fe92f583191011865121
SlyFabi <10423192+SlyFabi@users.noreply.github.com>
2020-07-15 22:47:16 +0200

    [Client] Limit to single instance

--- VfioUsbClient/Program.cs
+++ VfioUsbClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,26 @@ namespace VfioUsbClient
 {
     static class Program
     {
+        private static readonly string INSTANCE_MUTEX = "Global\\VfioUsbManager";
+
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            using (Mutex mutex = new Mutex(false, INSTANCE_MUTEX))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("An instance is already running !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainWindow());
+            }
         }
     }
 }
